ba56c7e74c6035a6195a4dfc53c02ceac4e83b50
Sam Ware <saware@mymail.mines.edu>
2020-09-07 14:28:42 -0600

    Can successfully plot an approximated gaussian curve on the xrd data

--- PeakFitting/singleXtalcurve.py
+++ PeakFitting/singleXtalcurve.py
@@ -6,16 +6,23 @@ import numpy as np
 df = pd.read_csv('singleXtalnum.csv') #read the csv file
 twoTheta = df["Angle"] #assigns angle column
 intensity = df["Intensity"] #assigns intensity column 
+
 maxIntensity = intensity.max() #finds the value of the max intensity
-sigma = intensity.std() #find the standard deviation
-#print(intensity[maxIntensity])
+peakLocation = twoTheta[intensity.idxmax()] #finds the 2-theta value of the maximum
 
+derivativeIntensity = intensity.diff() #finds the rate of change of intensity, hopefully we can use this to find a good range for sigma
+print(derivativeIntensity[5851:5880]) #roughly where the peaks is
 
+sigma = 0.01 #placeholder value, need to figure out how to find sigma over a smaller range
 
+FWHM = 2*sigma*np.sqrt(2*np.log(2)) #calcualtes the full width at half maximum using standard deviation
+peak_move = twoTheta - peakLocation #shifts the peak to the maximum
+G = maxIntensity*np.e**(-4*np.log(2)*((peak_move/FWHM)**2)) #calculates gaussian curve based on dataframe
 
-#def gaussian(sigma, peak, ):
-
+#plots everything
 plt.plot(twoTheta, intensity, label = 'Single Crystal Si')
+plt.plot(twoTheta, G, label = 'Gaussian')
+plt.plot(twoTheta, derivativeIntensity, label = 'Rate of Change')
 plt.xlabel("Two Theta")
 plt.ylabel("Intensity")
 plt.legend()
