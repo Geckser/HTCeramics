6014288fc82a569b3adb5890ea91cbe9c4c8e14b
Sam Ware <saware@mymail.mines.edu>
2020-09-09 16:34:37 -0600

    Added functions to make the code more readible

--- PeakFitting/singleXtalcurve.py
+++ PeakFitting/singleXtalcurve.py
@@ -14,49 +14,42 @@ maxIntensity = intensity.max() #finds the value of the max intensity
 peakLocation = twoTheta[intensity.idxmax()] #finds the 2-theta value of the maximum
 derivativeIntensity = intensity.diff() #finds the rate of change of intensity, hopefully we can use this to find a good range for sigma
 
-for i in range(0, len(derivativeIntensity)): #finds where the spike begins
-    if derivativeIntensity[i] >= 100: #this number might be specific to the curve, probably need a more universal way of finding it
-        curveStart = i
-        break
+def curveFinder(): #funtion to narrow down the area of the peak
+    for i in range(0, len(derivativeIntensity)): #finds where the spike begins
+        if derivativeIntensity[i] >= 100: #this number might be specific to the curve, probably need a more universal way of finding it
+            curveStart = i
+            break
 
 
-for j in range(intensity.idxmax()+1, len(derivativeIntensity)): #finds where the curve starts sloping up again. plus one is needed because .diff has one less index
-    if derivativeIntensity[j] > 0:
-        curveEnd = j
-        break
+    for j in range(intensity.idxmax()+1, len(derivativeIntensity)): #finds where the curve starts sloping up again. plus one is needed because .diff has one less index
+        if derivativeIntensity[j] > 0:
+            curveEnd = j
+            break
+    
+    peakArea = pd.DataFrame({'Angle': twoTheta[curveStart:curveEnd], 'Intensity': intensity[curveStart:curveEnd]}) #limits the area of the data around the curve for more consistent matching
+    peakArea = peakArea.reset_index() #resets index so the fitting works 
+    boundTwoTheta = peakArea["Angle"]
+    boundIntensity = peakArea["Intensity"]
+    #print(peakArea)
+    return boundTwoTheta, boundIntensity
 
-peakArea = pd.DataFrame({'Angle': twoTheta[curveStart:curveEnd], 'Intensity': intensity[curveStart:curveEnd]}) #limits the area of the data around the curve for more consistent matching
-peakArea = peakArea.reset_index() #resets index so the fitting works 
-boundTwoTheta = peakArea["Angle"]
-boundIntensity = peakArea["Intensity"]
-#print(peakArea)
+def fittingGaussian(x, y): #fits a gaussian curve
+    mod = GaussianModel() 
+    pars = mod.guess(y, x = x) #does the gaussian fit
+    out = mod.fit(y, pars, x = x)
+    print(out.fit_report(min_correl=0.25))
+    return out.best_fit
 
+boundedData = curveFinder()
 
+x = boundedData[0] #for some reason the fit only works if the data is assigned like this
+y = boundedData[1]
 
-x = boundTwoTheta #for some reason the fit only works if the data is assigned like this
-y = boundIntensity
-
-mod = LorentzianModel() 
-
-pars = mod.guess(y, x = x) #does the gaussian fit
-out = mod.fit(y, pars, x = x)
-print(out.fit_report(min_correl=0.25))
-fittingParams = out.params
-#print(fittingParams[sigma])
-
-
-""" This is an attempt to get sigma from the fitting curve. WIP
-print()
-file = open('fittingParams.txt' , 'w')
-file.write(str(out.params))
-file.close()
-"""
+gaussianFit = fittingGaussian(x, y)
 
 #plots everything
 plt.plot(twoTheta, intensity, label = 'Single Crystal Si')
-#plt.plot(twoTheta, G, label = 'Gaussian')
-plt.plot(boundTwoTheta, out.best_fit, label = 'Fitted Curve')
-#plt.plot(twoTheta, derivativeIntensity, label = 'Rate of Change')
+plt.plot(boundedData[0], gaussianFit, label = 'Gaussian Fitted Curve')
 plt.xlabel("Two Theta")
 plt.ylabel("Intensity")
 plt.legend()
