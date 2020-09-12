7f571d39ab27b122c4298c1546fbb0c429294660
Sam Ware <saware@mymail.mines.edu>
2020-09-12 12:31:08 -0600

    added a simple file selection

--- PeakFitting/dataPlotting.py
+++ PeakFitting/dataPlotting.py
@@ -3,7 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv('xrdData/915aluminumNum.csv')
+def plotChooser():
+    print("[1] Load new file\n[2] Load Default")
+    choice = int(input("Input Choice: "))
+    if choice == 1: #good for loading a file once
+        dataFile = input('Enter name of file: ')
+        dataFile = str(dataFile)
+    elif choice == 2: #if a file needs to be loaded repeatedly, put it here
+        dataFile = 'polyxtal.csv'
+    else:
+        print("Invalid Choice")
+    return dataFile
+
+csvFile = plotChooser()
+filePath = "xrdData/" + csvFile
+print(filePath)
+
+df = pd.read_csv(filePath)
 twoTheta = df["Angle"]
 intensity = df["Intensity"]
 
