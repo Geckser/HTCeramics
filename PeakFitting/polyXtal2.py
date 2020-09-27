809c17adbd6a16d551aeab91da4434f8153cddbe
Sam Ware <saware@mymail.mines.edu>
2020-09-27 16:44:56 -0600

    fits curves to all the peaks sometimes

--- PeakFitting/polyXtal2.py
+++ PeakFitting/polyXtal2.py
@@ -11,9 +11,10 @@ df = pd.read_csv('xrdData/polyxtal.csv') #read the csv file
 twoTheta = df["Angle"] #assigns angle column
 intensity = df["Intensity"] #assigns intensity column 
 
+
 #This function made by Chris Ostrouchov, https://chrisostrouchov.com/post/peak_fit_xrd_python/
 def generateModel(spec):
-    compositeModel = None
+    composite_model = None
     params = None
     x = spec['x']
     y = spec['y']
@@ -21,32 +22,35 @@ def generateModel(spec):
     x_max = np.max(x)
     x_range = x_max - x_min
     y_max = np.max(y)
-    
-    for i, basis_func in enumerate(spec['model']): #builds the models around the scattered data
+    for i, basis_func in enumerate(spec['model']):
         prefix = f'm{i}_'
-        model = getattr(models, basis_func['type'])(prefix = prefix)
-        if basis_func['type'] in ['GaussianModel', 'Lorentzian Model', 'VoigtModel']:
-            model.set_param_hint('sigma', min = 1e-6, max = x_range)
-            model.set_param_hint('center', min = x_min, max = x_max)
-            model.set_param_hint('height', min = 1e-6, max = 1.1*y_max)
-            model.set_param_hint('amplitude', min = 1e-6)
-            default_params = {prefix+'center':x_min + x_range*random.random(), prefix+'height': y_max*random.random(), prefix+'sigma': x_range*random.random()}
+        model = getattr(models, basis_func['type'])(prefix=prefix)
+        if basis_func['type'] in ['GaussianModel', 'LorentzianModel', 'VoigtModel', 'PseudoVoigtModel']: # for now VoigtModel has gamma constrained to sigma
+            model.set_param_hint('sigma', min=1e-6, max=x_range)
+            model.set_param_hint('center', min=x_min, max=x_max)
+            model.set_param_hint('height', min=1e-6, max=1.1*y_max)
+            model.set_param_hint('amplitude', min=1e-6)
+            # default guess is horrible!! do not use guess()
+            default_params = {
+                prefix+'center': x_min + x_range * random.random(),
+                prefix+'height': y_max * random.random(),
+                prefix+'sigma': x_range * random.random()
+            }
         else:
             raise NotImplemented(f'model {basis_func["type"]} not implemented yet')
-        if 'help' in basis_func:
-            for param, option in basis_func['help'].items():
-                model.set_param_hint(param, **option)
+        if 'help' in basis_func:  # allow override of settings in parameter
+            for param, options in basis_func['help'].items():
+                model.set_param_hint(param, **options)
         model_params = model.make_params(**default_params, **basis_func.get('params', {}))
         if params is None:
             params = model_params
         else:
             params.update(model_params)
-        if compositeModel is None:
-            compositeModel = model
+        if composite_model is None:
+            composite_model = model
         else:
-            compositeModel = compositeModel + model
-    return compositeModel, params
-
+            composite_model = composite_model + model
+    return composite_model, params
 
 
 def peakFinder(spec, endLastPeak): #finds and counts peaks
@@ -105,16 +109,18 @@ def updateParams(spec): #updates limit from data found in multiPeakFinder
     peakData = multiPeakFinder(spec)
     peak_indicies = peakData['Max']
     peak_widths = peakData['End'] - peakData['Start']
+    #peak_widths = [peakData['End'],peakData['Start']]
     model_indicies = range(0, len(peak_indicies))
 
     for peak_indicie, model_indicie in zip(peak_indicies.tolist(), model_indicies): #This block is by by Chris Ostrouchov, https://chrisostrouchov.com/post/peak_fit_xrd_python/
         model = spec['model'][model_indicie]
-        if model['type'] in ['GaussianModel', 'LorentzianModel', 'VoigtModel']:
+        if model['type'] in ['GaussianModel', 'LorentzianModel', 'VoigtModel', 'PseudoVoigtModel']:
             params = {
                 'height': y[peak_indicie],
                 'sigma': x_range / len(x) * np.min(peak_widths),
                 'center': x[peak_indicie]
             }
+            print(params)
             if 'params' in model:
                 model.update(params)
             else:
@@ -123,29 +129,40 @@ def updateParams(spec): #updates limit from data found in multiPeakFinder
             raise NotImplemented(f'model {basis_func["type"]} not implemented yet')
     return peak_indicies
     
-
-spec = {'x':twoTheta, 'y':intensity, 'model':[
-    {'type': 'GaussianModel'}, 
-    {'type': 'GaussianModel'}, 
+    
+defaultspec = {'x':twoTheta, 'y':intensity, 'model':[
     {'type': 'GaussianModel'}, 
     {'type': 'GaussianModel'},
     {'type': 'GaussianModel'},
-    {'type': 'GaussianModel'}
+    {'type': 'GaussianModel'},
+    {'type': 'GaussianModel'},
+    {'type': 'GaussianModel'},
+    {'type': 'GaussianModel'},
+    {'type': 'GaussianModel'},
+    {'type': 'GaussianModel'},
     ]}
 
-foundPeaks = updateParams(spec)
+spec = defaultspec
 
+foundPeaks = updateParams(spec)
+model, params = generateModel(spec)
+
+output = model.fit(spec['y'], params, x=spec['x'])
+#print_best_values(spec, output)
+
+#plotting stuff below here
+"""
 fig, ax = plt.subplots()
 ax.scatter(spec['x'], spec['y'], s=4)
 for i in foundPeaks:
     ax.axvline(x=spec['x'][i], c='black', linestyle='dotted')
+"""
+output.plot(data_kws={'markersize':  1})
 
-ax.axhline(y = intensity.mean(), c='red', linestyle = 'dotted')
-ax.axhline(y = 5*intensity.mean(), c='red', linestyle = 'dotted')
+#ax.axhline(y = intensity.mean(), c='red', linestyle = 'dotted')
+#ax.axhline(y = 5*intensity.mean(), c='red', linestyle = 'dotted')
 
-model, params = generateModel(spec)
-output = model.fit(spec['y'], params, x=spec['x'])
-fig, gridspec = output.plot(data_kws={'markersize':  1})
+#fig, gridspec = output.plot(data_kws={'markersize':  1})
 
 plt.show()
 
@@ -174,3 +191,4 @@ def updateSpecFromPeaks(spec, model_indicies, peak_widths=(10, 25), **kwargs): #
         else:
             raise NotImplemented(f'model {basis_func["type"]} not implemented yet')
     return peak_indicies
+"""
