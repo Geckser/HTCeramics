92d3dcc70dbd506cacc1275b9c28e6344bc5be7a
Sam Ware <saware@mymail.mines.edu>
2020-10-03 11:31:16 -0600

    lorentzian support working

--- PeakFitting/polyXtal2.py
+++ PeakFitting/polyXtal2.py
@@ -25,7 +25,7 @@ def createModel(spec, params):
     pars = gaussi.make_params() #creates params dataset
     
     for i in peaks:
-        if basis_func[i] == 'GaussianModel':
+        if basis_func[i] == 'GaussianModel': #checks if the peak is gaussian
             prefix = 'g'+str(i)+'_'
             gauss = models.GaussianModel(prefix = prefix)
             pars.update(gauss.make_params())
@@ -33,10 +33,25 @@ def createModel(spec, params):
             pars[prefix + 'center'].set(center[i])
             pars[prefix + 'sigma'].set(sigma[i])
             pars[prefix + 'amplitude'].set(height[i])
-            if i == 0:
+            if i == 0: #initiallizes the mod variable if it's the first model in the composite function
                 mod = gauss
             else:
-                mod = mod + gauss   
+                mod = mod + gauss
+                
+        elif basis_func[i] == 'LorentzianModel':
+            prefix = 'l'+str(i)+'_'
+            lorentz = models.LorentzianModel(prefix = prefix)
+            pars.update(lorentz.make_params())
+
+            pars[prefix + 'center'].set(center[i])
+            pars[prefix + 'sigma'].set(sigma[i])
+            pars[prefix + 'amplitude'].set(height[i])
+            if i == 0: #initiallizes the mod variable if it's the first model in the composite function
+                mod = lorentz
+            else:
+                mod = mod + lorentz  
+
+        
       
     return mod, pars
 
@@ -116,7 +131,7 @@ defaultspec = {'x':twoTheta, 'y':intensity, 'model':[
     {'type': 'GaussianModel'}, 
     {'type': 'GaussianModel'},
     {'type': 'GaussianModel'},
-    {'type': 'GaussianModel'},
+    {'type': 'LorentzianModel'},
     {'type': 'GaussianModel'},
     {'type': 'GaussianModel'},
     ]}
@@ -126,95 +141,20 @@ spec = defaultspec
 foundPeaks, params = updateParams(spec)
 mod, pars = createModel(spec, params)
 
-x = twoTheta
-y = intensity
-out = mod.fit(y, pars, x= x)
-out.plot(data_kws={'markersize':  1})
+out = mod.fit(intensity, pars, x= twoTheta)
 
-
-#output = model.fit(spec['y'], params, x=spec['x'])
-#print_best_values(spec, output)
 
 #plotting stuff below here
-"""
+out.plot(data_kws={'markersize':  1})
+
+""" This plots where the peaks are. Useful for testing
 fig, ax = plt.subplots()
 ax.scatter(spec['x'], spec['y'], s=4)
 for i in foundPeaks:
     ax.axvline(x=spec['x'][i], c='black', linestyle='dotted')
 """
-#output.plot(data_kws={'markersize':  1})
 
-#ax.axhline(y = intensity.mean(), c='red', linestyle = 'dotted')
-#ax.axhline(y = 5*intensity.mean(), c='red', linestyle = 'dotted')
-
-#fig, gridspec = output.plot(data_kws={'markersize':  1})
 
 plt.show()
 
-"""
-Legacy Code: 
 
-#This function made by Chris Ostrouchov, https://chrisostrouchov.com/post/peak_fit_xrd_python/
-def generateModel(spec):
-    composite_model = None
-    params = None
-    x = spec['x']
-    y = spec['y']
-    x_min = np.min(x)
-    x_max = np.max(x)
-    x_range = x_max - x_min
-    y_max = np.max(y)
-    for i, basis_func in enumerate(spec['model']):
-        prefix = f'm{i}_'
-        model = getattr(models, basis_func['type'])(prefix=prefix)
-        if basis_func['type'] in ['GaussianModel', 'LorentzianModel', 'VoigtModel', 'PseudoVoigtModel']: # for now VoigtModel has gamma constrained to sigma
-            model.set_param_hint('sigma', min=1e-6, max=x_range)
-            model.set_param_hint('center', min=x_min, max=x_max)
-            model.set_param_hint('height', min=1e-6, max=1.1*y_max)
-            model.set_param_hint('amplitude', min=1e-6)
-            # default guess is horrible!! do not use guess()
-            default_params = {
-                prefix+'center': x_min + x_range * random.random(),
-                prefix+'height': y_max * random.random(),
-                prefix+'sigma': x_range * random.random()
-            }
-        else:
-            raise NotImplemented(f'model {basis_func["type"]} not implemented yet')
-        if 'help' in basis_func:  # allow override of settings in parameter
-            for param, options in basis_func['help'].items():
-                model.set_param_hint(param, **options)
-        model_params = model.make_params(**default_params, **basis_func.get('params', {}))
-        if params is None:
-            params = model_params
-        else:
-            params.update(model_params)
-        if composite_model is None:
-            composite_model = model
-        else:
-            composite_model = composite_model + model
-    return composite_model, params
-
-
-#Funtion by Chris Ostrouchov, https://chrisostrouchov.com/post/peak_fit_xrd_python/, guesses using find_peaks_cwt
-def updateSpecFromPeaks(spec, model_indicies, peak_widths=(10, 25), **kwargs): #guesses where the peaks are 
-    x = spec['x']
-    y = spec['y']
-    x_range = np.max(x) - np.min(x)
-    peak_indicies = signal.find_peaks_cwt(y, peak_widths)
-    np.random.shuffle(peak_indicies)
-    for peak_indicie, model_indicie in zip(peak_indicies.tolist(), model_indicies):
-        model = spec['model'][model_indicie]
-        if model['type'] in ['GaussianModel', 'LorentzianModel', 'VoigtModel']:
-            params = {
-                'height': y[peak_indicie],
-                'sigma': x_range / len(x) * np.min(peak_widths),
-                'center': x[peak_indicie]
-            }
-            if 'params' in model:
-                model.update(params)
-            else:
-                model['params'] = params
-        else:
-            raise NotImplemented(f'model {basis_func["type"]} not implemented yet')
-    return peak_indicies
-"""
